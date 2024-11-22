65816bbde0684410cb84d477f1aef741691aeb7a
maryammuhammadafzal <natiqnagori7@gamil.com>
2024-11-22 19:40:02 +0500

    updated

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ const welcomePage = document.getElementById('welcome-page');
 const mainContent = document.getElementById('main-content');
 const getStartedButton = document.getElementById('get-started');
 
-// Optionally allow manual navigation with "Get Started" button
-getStartedButton.addEventListener('click', () => {
-  welcomePage.classList.add('hidden');
-  mainContent.classList.remove('hidden');
-});
-
 document.querySelector('.contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
   alert('Thank you for contacting us! We’ll get back to you soon.');
 });
+
+let signinBtn = document.getElementById('signinBtn');
+signinBtn.addEventListener('click', function(){
+  window.location.href = "./loginform/login.html"
+})
